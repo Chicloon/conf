1f24e097460eb03b03566d2446efac6993deba6c
Efim Khlebnyy <Chicloon@gmail.com>
2016-06-14 16:28:49 +0900

    added foundation 6 framework

--- client/Layouts/MainLayout.jsx
+++ client/Layouts/MainLayout.jsx
@@ -2,12 +2,19 @@ import React from 'react';
 // import AccountsUI from '../AccountsUI.jsx';
 
 export const MainLayout = ({content}) => (
-  <div>
-    
-    <h1>Hello World</h1>
+    <div>
+        <div className="row">
+            <div className="small-6 columns">6 columns</div>
+            <div className="small-6 columns">6 columns</div>
+        </div>
+        <div className="row">
+            <div className="medium-6 large-4 columns">12/6/4 columns</div>
+            <div className="medium-6 large-8 columns">12/6/8 columns</div>
+        </div>
+        <h1>Hello World</h1>
 
-    <main>
-      {content}
-    </main>
-  </div>
+        <main>
+            {content}
+        </main>
+    </div>
 );
--- client/Routs.jsx
+++ client/Routs.jsx
@@ -4,6 +4,8 @@ import {mount} from 'react-mounter';
 import HomePage from './Pages/HomePage.jsx';
 import {MainLayout} from './Layouts/MainLayout.jsx';
 
+console.log ("I'm alive");
+
 FlowRouter.route('/', {
   action() {
     mount(MainLayout, {
