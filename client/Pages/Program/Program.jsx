1cb2f383190bfb75567805d27365c276ddb84a39
Chicloon@gmail.com <gfhjkm1982>
2016-08-21 20:35:22 +0900

    initial conf on base of angular2-express-starter

--- client/Pages/Program/Program.jsx
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react';
-
-export default class Program extends Component {
-  render(){
-    return (
-      <h1>Program page</h1>
-    )
-  }
-}
--- server/publish.js
+++
@@ -1,7 +0,0 @@
-console.log("I'm a live");
-
-Participants = new Mongo.Collection ("participants");
-
-Meteor.publish("allAbstracts", function (){
-  return Participants.find();
-});
