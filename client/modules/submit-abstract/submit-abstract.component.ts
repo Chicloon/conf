4d5dd5ba57559129249cc4c881e7d06620015bd7
Chicloon <Chicloon@gmail.com>
2016-08-30 08:19:17 +0900

    playing with forms 2.0

--- client/modules/submit-abstract/submit-abstract.component.ts
+++ client/modules/submit-abstract/submit-abstract.component.ts
@@ -12,19 +12,26 @@ import { AbstractsService } from '../abstracts/abstracts.service';
 export class SubmitAbstractComponent implements OnInit {
     form: FormGroup;
     searchControl = new FormControl();
+
     // form = new FormGroup({
     //     title: new FormControl(),
     //     content: new FormControl()
     // });
 
+    title = new FormControl('this is a title');
+    content = new FormControl();
     constructor(private _abstracts: AbstractsService, private formBuilder: FormBuilder) { }
 
 
     ngOnInit(){
-         this.form = this.formBuilder.group({
-             title: '',
-             content: ['This is a content', Validators.required]
-         });
+        //  this.form = this.formBuilder.group({
+        //      title: '',
+        //      content: ['This is a content', Validators.required]
+        //  });
+        this.form = this.formBuilder.group({
+            title: this.title,
+            content: this.content
+        });
         this.searchControl.valueChanges.subscribe (value => {
             console.log(value);
             console.log(this.form.valid);
@@ -37,7 +44,8 @@ export class SubmitAbstractComponent implements OnInit {
         });
 
         console.log(this.form.controls);
-        console.log (this.form.controls.title.value); 
+        // console.log (this.form.controls.title.value); 
+        console.log(this.title.value);
     }
 
     
