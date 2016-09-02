1a796d0a8696befb068ab626d372e20dc0ec79af
Chicloon <Chicloon@gmail.com>
2016-09-02 18:02:46 +0900

    forms 0.1

--- client/modules/submit-abstract/submit-abstract.component.ts
+++ client/modules/submit-abstract/submit-abstract.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { Abstract } from '../abstracts/abstract';
 import { FormGroup, FormControl,FormBuilder, Validators } from '@angular/forms';
+import {NgClass} from '@angular/common';
+
 
 import { AbstractsService } from '../abstracts/abstracts.service';
 
@@ -8,33 +10,37 @@ import { AbstractsService } from '../abstracts/abstracts.service';
 @Component({
     selector: "my-submit-abstract-component",
     templateUrl: `client/modules/submit-abstract/submit-abstract.component.html`,
-    styleUrls:['client/modules/submit-abstract/submit-abstract.component.css']
+    styleUrls:['client/modules/submit-abstract/submit-abstract.component.css'],
+    directives: [NgClass]
 })
 export class SubmitAbstractComponent implements OnInit {
     form: FormGroup;
     private searchfieldCharactersCounter = 0;
 
-    private title = new FormControl('',Validators.required);
-    private content = new FormControl();
+    private title = new FormControl('', Validators.compose([Validators.pattern('^[a-zA-Z0-9_ ]*$'),Validators.required]));
+    
+    private content = new FormControl('',Validators.compose([Validators.pattern('^[a-zA-Z0-9_ ]*$'),Validators.required]));
+    // private content = new FormControl();
     private section = new FormControl();
+
+    private trigger:Boolean = false;
+
+    
     constructor(private _abstracts: AbstractsService, private formBuilder: FormBuilder) { }
 
 
     ngOnInit(){
-        //  this.form = this.formBuilder.group({
-        //      title: '',
-        //      content: ['This is a content', Validators.required]
-        //  });
+        
         this.form = this.formBuilder.group({
             title: this.title,
             content: this.content,
             section: this.section
         });
-        this.content.valueChanges.subscribe (value => this.contentfieldValidation(value) );
+        this.content.valueChanges.subscribe (value => this.contentfieldValidation(value));
+        // this.form.valueChanges.subscribe(() => console.log(this.form.valid));
 
         console.log(this.form.controls);
-        console.log(this.form.invalid);
-        // console.log (this.form.controls.title.value); 
+        console.log(this.form.invalid); 
         console.log(this.title.value);
     }
 
@@ -48,7 +54,17 @@ export class SubmitAbstractComponent implements OnInit {
                 this.title.setValue('test');
                 
             }
-            this.title.setValue('this is a title')
+            this.form.valid;
+    }
+
+    lostFocus () {
+        if (this.searchfieldCharactersCounter < 10) {
+            this.trigger = true;
+        }               
+    }
+
+    inFocus () {
+        this.trigger = false;
     }
     
     onSubmit() {
